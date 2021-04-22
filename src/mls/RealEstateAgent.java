4f4cea114c7ca3699e36fed302369bc2e29bf2f4
hp <hp@DESKTOP-VP76K81.phub.net.cable.rogers.com>
2021-04-22 19:15:54 -0400

    First Commit

---
+++ src/mls/RealEstateAgent.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mls;
+
+/**
+ *
+ * @author Vansh
+ */
+public class RealEstateAgent  {
+    int agentNumber;
+    String name;
+    String officePhone;
+    String cellPhone;
+    String email;
+}
---
+++ src/mls/Structure.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mls;
+
+/**
+ *
+ * @author Vansh
+ */
+public class Structure {
+    String dateBuilt;
+    Double area;
+    int numberOfBedrooms;
+    int numberOfBathrooms;
+}
